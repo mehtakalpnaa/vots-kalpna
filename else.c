5ee23e433aae9c7f9c406f1aa2e955118ef27b98
mehtakalpnaa <kalpnamehtak@gmail.com>
2024-07-03 02:38:29 -0700

    even numbers

---
+++ else.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int number ;
+
+    printf("enter any number :");
+    scanf("%d",&number);
+
+    if(number %2 ==0)
+    {
+
+        printf("enter any even number");
+        
+    }
+    else{
+        printf("this is not even number");
+    }
+    return 0;
+
+
+}
